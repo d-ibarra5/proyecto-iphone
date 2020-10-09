bbbb3cb97563e2ed6e17a4fe08c18385084a1da3
David <test@gmail.com>
2020-10-09 11:33:55 -0700

    Creacion de pantallas de "Inicio de sesión" y "Recuperar password"

---
+++ InstaISIL/InicioSesionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InicioSesionViewController.swift
+//  InstaISIL
+//
+//  Created by user178963 on 10/9/20.
+//  Copyright © 2020 isil. All rights reserved.
+//
+
+import UIKit
+
+class InicioSesionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ InstaISIL/RecuperarPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecuperarPasswordViewController.swift
+//  InstaISIL
+//
+//  Created by user178963 on 10/9/20.
+//  Copyright © 2020 isil. All rights reserved.
+//
+
+import UIKit
+
+class RecuperarPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
