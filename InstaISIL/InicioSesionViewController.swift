f6bfbf21a040585241526bdcf277f69409673e0c
David <test@gmail.com>
2020-10-09 11:53:37 -0700

    Relacion entre pantallas agregadas, eventos de teclado para "Inicio de sesión" y "Recuperar password"

--- InstaISIL/InicioSesionViewController.swift
+++ InstaISIL/InicioSesionViewController.swift
@@ -9,12 +9,64 @@
 import UIKit
 
 class InicioSesionViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var constraintBottomScroll: NSLayoutConstraint!
+    
+    func MensajeAlerta (titulo: String, mensaje: String) {
+        let alerta = UIAlertController(title: titulo, message: mensaje, preferredStyle: .alert)
+        alerta.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alerta, animated: true)
     }
     
+    @IBAction func clickBtnCloseKeyboard(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+         super.viewWillAppear(animated)
+         
+         NotificationCenter.default.addObserver(self,
+                                                selector: #selector(self.keyboardWillShow(_:)),
+                                                name: UIResponder.keyboardWillShowNotification,
+                                                object: nil)
+         
+         NotificationCenter.default.addObserver(self,
+                                                selector: #selector(self.keyboardWillHide(_:)),
+                                                name: UIResponder.keyboardWillHideNotification,
+                                                object: nil)
+     }
+     
+     override func viewWillDisappear(_ animated: Bool) {
+         super.viewWillDisappear(animated)
+         NotificationCenter.default.removeObserver(self)
+     }
+     
+     @objc func keyboardWillShow(_ notification: Notification) {
+         
+         let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+         let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+         
+         UIView.animate(withDuration: animationDuration) {
+             
+             self.constraintBottomScroll.constant = keyboardFrame.height + 35
+             self.view.layoutIfNeeded()
+         }
+     
+     }
+     
+     @objc func keyboardWillHide(_ notification: Notification) {
+    
+         let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+         
+         UIView.animate(withDuration: animationDuration) {
+             
+             self.constraintBottomScroll.constant = 35
+             self.view.layoutIfNeeded()
+         }
+     }
 
 }
--- InstaISIL/RecuperarPasswordViewController.swift
+++ InstaISIL/RecuperarPasswordViewController.swift
@@ -9,22 +9,64 @@
 import UIKit
 
 class RecuperarPasswordViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+  
+    @IBOutlet weak var constraintBottomScroll: NSLayoutConstraint!    
+    
+    func MensajeAlerta (titulo: String, mensaje: String) {
+        let alerta = UIAlertController(title: titulo, message: mensaje, preferredStyle: .alert)
+        alerta.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alerta, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func clickBtnCloseKeyboard(_ sender: Any) {
+        self.view.endEditing(true)
     }
-    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+         super.viewWillAppear(animated)
+         
+         NotificationCenter.default.addObserver(self,
+                                                selector: #selector(self.keyboardWillShow(_:)),
+                                                name: UIResponder.keyboardWillShowNotification,
+                                                object: nil)
+         
+         NotificationCenter.default.addObserver(self,
+                                                selector: #selector(self.keyboardWillHide(_:)),
+                                                name: UIResponder.keyboardWillHideNotification,
+                                                object: nil)
+     }
+     
+     override func viewWillDisappear(_ animated: Bool) {
+         super.viewWillDisappear(animated)
+         NotificationCenter.default.removeObserver(self)
+     }
+     
+     @objc func keyboardWillShow(_ notification: Notification) {
+         
+         let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+         let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+         
+         UIView.animate(withDuration: animationDuration) {
+             
+             self.constraintBottomScroll.constant = keyboardFrame.height + 35
+             self.view.layoutIfNeeded()
+         }
+     
+     }
+     
+     @objc func keyboardWillHide(_ notification: Notification) {
+    
+         let animationDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double ?? 0
+         
+         UIView.animate(withDuration: animationDuration) {
+             
+             self.constraintBottomScroll.constant = 35
+             self.view.layoutIfNeeded()
+         }
+     }
 
 }
