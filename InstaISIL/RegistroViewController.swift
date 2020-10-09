ac3bc6e996a51fb06e9b50a364ea09bf81dc2cbb
David <test@gmail.com>
2020-10-09 10:56:10 -0700

    Validation de campos de la pantalla de registro a la hora de presionar botón de "Registrar"

--- InstaISIL/RegistroViewController.swift
+++ InstaISIL/RegistroViewController.swift
@@ -11,15 +11,86 @@ import UIKit
 class RegistroViewController: UIViewController {
     
     @IBOutlet weak var pickerCarrera: UIPickerView!
-    
     @IBOutlet weak var pickerSede: UIPickerView!
-    
-    
     @IBOutlet weak var constraintBottomScroll: NSLayoutConstraint!
+    
+    @IBOutlet weak var txtNombres: UITextField!
+    @IBOutlet weak var txtApellidos: UITextField!
+    @IBOutlet weak var txtUsuario: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
     
     let sedes = ["Jesus Maria", "Miraflores", "La Molina", "San Isidro"]
     
     let carreras = ["Diseno Grafico","Periodismo","Marketing","Desarrollo de Software","Hoteleria"]
+    
+    func MensajeAlerta (titulo: String, mensaje: String) {
+        let alerta = UIAlertController(title: titulo, message: mensaje, preferredStyle: .alert)
+        alerta.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alerta, animated: true)
+    }
+    
+    func ValidarConRegex(patron: String, str: String) -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: patron, options: .caseInsensitive)
+            if regex.matches(in: str, options: [], range: NSMakeRange(0, str.count)).count > 0 {return true}
+        }
+        catch {}
+        return false
+    }
+    
+    @IBAction func clickBtnRegistrar(_ sender: Any) {
+        
+        //No permitir campos vacios
+        guard let nombres = txtNombres.text, !nombres.isEmpty else {
+            MensajeAlerta(titulo: "Nombre vacio", mensaje: "Debe insertar sus nombres")
+            return
+        }
+        guard let apellidos = txtApellidos.text, !apellidos.isEmpty else {
+            MensajeAlerta(titulo: "Apellido vacio", mensaje: "Debe insertar sus apellidos")
+            return
+        }
+        guard let usuario = txtUsuario.text, !usuario.isEmpty else {
+            MensajeAlerta(titulo: "Usuario vacio", mensaje: "Debe insertar su nombre de usuario")
+            return
+        }
+        guard let password = txtPassword.text, !password.isEmpty else {
+            MensajeAlerta(titulo: "Password vacio", mensaje: "Debe insertar su password")
+            return
+        }
+                
+        //Validar caracteres con regex
+        let usuarioCorrecto = ValidarConRegex(patron: "^[0-9a-zA-Z\\_]{7,18}$", str: usuario)
+        let passwordCorrecto = ValidarConRegex(patron: "^[0-9a-zA-Z\\_]{7,18}$", str: password)
+        let nombreIncorrecto = ValidarConRegex(patron: ".*[^A-Za-z ].*", str: nombres)
+        let apellidoIncorrecto = ValidarConRegex(patron: ".*[^A-Za-z ].*", str: apellidos)
+        
+        
+        if nombreIncorrecto == true {
+            MensajeAlerta(titulo: "Nombres no validos", mensaje: "Por favor solo ingresar letras en el nombre")
+            return
+        }
+        
+        if apellidoIncorrecto == true {
+            MensajeAlerta(titulo: "Apellidos no validos", mensaje: "Por favor solo ingresar letras en el apellido")
+            return
+        }
+        
+        if usuarioCorrecto == false {
+            MensajeAlerta(titulo: "Usuario no valido", mensaje: "Por favor solo ingresar letras, numeros o underscore. Ademas el usuario debe contener entre 7 y 18 caracteres.")
+            return
+        }
+        if passwordCorrecto == false {
+            MensajeAlerta(titulo: "Password no valido", mensaje: "Por favor solo ingresar letras, numeros o underscore. Ademas el password debe contener entre 7 y 18 caracteres.")
+            return
+        }
+        
+        //Si todo salio bien
+        MensajeAlerta(titulo: "Usuario registrado correctamente!", mensaje: "Bienvenido, \(usuario)! (\(nombres) \(apellidos)) \n" +
+        "Carrera: \(carreras[pickerCarrera.selectedRow(inComponent: 0)]) \n" +
+        "Sede: \(sedes[pickerSede.selectedRow(inComponent: 0)])")
+                
+    }
+    
     
     @IBAction func clickBtnCloseKeyboard(_ sender: Any) {
         self.view.endEditing(true)
